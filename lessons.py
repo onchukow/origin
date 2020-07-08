6399b6f51ae9152d7176594efffecd2425e7f41b
E.O <onchukov.evg@gmail.com>
2020-07-08 15:21:40 +0300

    Initial commit

---
+++ lessons.py
@@ -0,0 +1,76 @@
+#
+# congr = 'First 5 places in competitions: 1. ivanov 2. petrov 3. sidorov 4. orlov 5. sokolov'
+# start = congr.find('1')
+# end = congr.find('4')
+# top3 = congr[start:end]
+# #top3 = top3.upper()
+# result = 'Congratulation {} with wins!'.format(top3.upper())
+# print(result)
+#
+# friends = ['Max', 'Adam', 'Brian']
+# friends.append('Zillion')
+# print(friends)
+# print(len(friends))
+# print(friends.pop())
+# print(friends)
+# friends.remove('Adam')
+# print(friends)
+# del friends[0]
+# print(friends)
+#
+# hero = 'Superman'
+# if 'man' in hero:
+#     print('got')
+#
+# print('PYTHON CONTEST')
+# count = int(input('Amount of members: '))
+# i = count
+# members = []
+# while i > 0:
+#     name = input('What is name of #{}: '.format(i))
+#     members.append(name)
+#     i -= 1
+#
+# members.reverse()
+# result = members[:3]
+# result = 'Winners: {}. CONGRATULATIONS!'.format(result)
+# print(result)
+# print('In contest played:', sorted(members))
+#
+# for member in members:
+#     print(member)
+#
+# winners = ['Max', 'Leo', 'Viktor']
+#
+# for winner in winners:
+#     print(winner)
+#
+# for i in range(1,len(winners)+1):
+#    # print(i)
+#     print(i,')', winners[i-1])
+#
+# friend = {
+#     'name': 'Max',
+#     'age': 23
+# }
+#
+# print(friend)
+# print(type(friend))
+#
+# print(friend['age'])
+#
+# friend['has_car'] = True
+# print(friend)
+#
+# # del friend['age']
+# # print(friend)
+#
+# for key in  friend.keys():
+#     print(key)
+#
+# for val in friend.values():
+#     print(val)
+
+day = {
+    
+}
